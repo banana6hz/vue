c7b51806fb415e42cfb86db5d46edcae656fe66e
nicole <1758063490@qq.com>
2020-03-26 17:34:45 +0800

    -update

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import Vue from 'vue'
 import router from './router'
 import App from './app.vue'
 
-import './assets/styles/global.less'
+import './assets/styles/global.scss'
 // 创建一个Vue实例App, mount就是讲我们的App挂载到root这样一个根节点中去
 
 new Vue({
     router,
     render: (h) =>h(App)
 }).$mount('#app')
+
+//测试babel-loader
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     sayHello() {
+//         console.log(`Hello, my name is ${this.name}`);
+//     }
+// }
+// const person = new Person("Ben", 28);
+// person.sayHello();
